36213c37ff5d27e47f6ffa6494a661a5b629c420
Concourse <concourse@finbourne.com>
2024-04-05 15:14:40 +0000

    Updates for v2.0.17

--- sdk/Finbourne.Configuration.Sdk/Client/GlobalConfiguration.cs
+++ sdk/Finbourne.Configuration.Sdk/Client/GlobalConfiguration.cs
@@ -6,6 +6,7 @@
  */
 
 using System.Collections.Generic;
+using Finbourne.Configuration.Sdk.Extensions;
 
 namespace Finbourne.Configuration.Sdk.Client
 {
--- sdk/Finbourne.Configuration.Sdk/Client/IReadableConfiguration.cs
+++ sdk/Finbourne.Configuration.Sdk/Client/IReadableConfiguration.cs
@@ -9,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
+using Finbourne.Configuration.Sdk.Extensions;
 using Finbourne.Configuration.Sdk.Client.Auth;
 
 namespace Finbourne.Configuration.Sdk.Client
